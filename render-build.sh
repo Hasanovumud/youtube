96a9cbb6667f8589a24d5caee345020134a82e7d
Hasanovumud <164156794+Hasanovumud@users.noreply.github.com>
2026-05-10 06:48:16 -0700

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+
+# FFmpeg static build yükləyirik
+mkdir -p bin
+curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar -xJ --strip-components=1 -C bin
